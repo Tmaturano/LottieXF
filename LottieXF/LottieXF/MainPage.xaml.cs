af980ef1fbb076c3252f57b9023f7d18f9b526fa
Thiago Maturana <thiagomatu@hotmail.com>
2018-01-10 20:28:28 -0200

    Add project files.

---
+++ LottieXF/LottieXF/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace LottieXF
+{
+    public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+            //https://blog.verslu.is/xamarin/xamarin-forms-xamarin/xamarin-app-lottie-animations/
+            //Lottie Files: https://www.lottiefiles.com/
+            InitializeComponent();
+		}
+	}
+}
